6baec103e8f1a69d48c57d64b150623161e729d5
Ha The Tung <tunght13488@gmail.com>
2020-10-19 13:59:03 +0800

    deal with env var as string

--- src/config.js
+++ src/config.js
@@ -67,7 +67,7 @@ module.exports = {
   redis: {
     host: process.env.REDIS_HOST || '127.0.0.1',
     port: process.env.REDIS_PORT || 6379,
-    auth: process.env.REDIS_AUTH || false,
+    auth: (process.env.REDIS_AUTH === 'true') || false,
     db: process.env.REDIS_DB || 0
   },
 
